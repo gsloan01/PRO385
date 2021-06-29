0eb4ebae1a61bba5c2d9fa5814b372818167c16f
gsloan01 <60530127+gsloan01@users.noreply.github.com>
2021-06-29 09:10:36 -0600

    Controller Input

--- Assets/Scripts/Controller/Player/SpaceshipPlayer.cs
+++ Assets/Scripts/Controller/Player/SpaceshipPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpaceshipPlayer : MonoBehaviour
 {
@@ -15,25 +16,60 @@ public class SpaceshipPlayer : MonoBehaviour
 
     void Start()
     {
-        
+        GetComponent<PlayerInput>().onActionTriggered += HandleAction;
     }
-
-    void Update()
+    private void HandleAction(InputAction.CallbackContext context)
     {
-        //Movement
-        input.x = Input.GetAxis("Horizontal");
-        input.y = Input.GetAxis("Vertical");
-
-        transform.Translate(input * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Fire1"))
+        if (context.action.name == "Fire")
         {
             OnFire();
         }
+        if (context.action.name == "Move")
+        {
+            OnMove(context);
+        }
     }
 
-    void OnFire()
+
+    void Update()
+    {
+        ////Gamepad controls
+        //Gamepad gamepad = Gamepad.current;
+        //if (gamepad == null) return;
+
+        //input = gamepad.leftStick.ReadValue();
+        //if (gamepad.buttonSouth.wasPressedThisFrame)
+        //{
+        //    OnFire();
+        //}
+
+        ////Keyboard Controls
+        //input.x = Input.GetAxis("Horizontal");
+        //input.y = Input.GetAxis("Vertical");
+
+        transform.Translate(input * speed * Time.deltaTime);
+
+        //if(Input.GetButtonDown("Fire1"))
+        //{
+        //    OnFire();
+        //}
+    }
+
+    public void OnFire()
     {
         Instantiate(shot, transform.position, Quaternion.identity);
     }
+
+    //sending and broadcasting messages
+    public void OnMove(InputValue inputValue)
+    {
+        input = inputValue.Get<Vector2>();
+    }
+
+    //invoke unity events
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
 }
